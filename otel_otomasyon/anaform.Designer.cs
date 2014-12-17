354f61c783b5bcd941ef59a7589292898ad162bd
Nebi KAYA <nebikaya22@hotmail.com>
2014-12-17 16:15:44 +0200

    test

---
+++ otel_otomasyon/anaform.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace otel_otomasyon
+{
+    partial class anasayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(anasayfa));
+            this.menuler = new System.Windows.Forms.MenuStrip();
+            this.müşteriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniMüşteriGirişiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mevcutOlanMüşterilerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.odalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.doluOdalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.boşalacakOdalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.boşOdalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kişilikOdalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kişilikOdalarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.müşteriAramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuler.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuler
+            // 
+            this.menuler.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuler.ImageScalingSize = new System.Drawing.Size(16, 32);
+            this.menuler.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.müşteriToolStripMenuItem,
+            this.odalarToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.menuler.Location = new System.Drawing.Point(0, 0);
+            this.menuler.Name = "menuler";
+            this.menuler.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuler.Size = new System.Drawing.Size(1362, 40);
+            this.menuler.TabIndex = 1;
+            this.menuler.Text = "menuStrip1";
+            // 
+            // müşteriToolStripMenuItem
+            // 
+            this.müşteriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniMüşteriGirişiToolStripMenuItem,
+            this.mevcutOlanMüşterilerToolStripMenuItem,
+            this.müşteriAramaToolStripMenuItem});
+            this.müşteriToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.müşteriToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("müşteriToolStripMenuItem.Image")));
+            this.müşteriToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.müşteriToolStripMenuItem.Name = "müşteriToolStripMenuItem";
+            this.müşteriToolStripMenuItem.Size = new System.Drawing.Size(124, 36);
+            this.müşteriToolStripMenuItem.Text = "Müşteri İşlemleri";
+            // 
+            // yeniMüşteriGirişiToolStripMenuItem
+            // 
+            this.yeniMüşteriGirişiToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("yeniMüşteriGirişiToolStripMenuItem.Image")));
+            this.yeniMüşteriGirişiToolStripMenuItem.Name = "yeniMüşteriGirişiToolStripMenuItem";
+            this.yeniMüşteriGirişiToolStripMenuItem.Size = new System.Drawing.Size(202, 38);
+            this.yeniMüşteriGirişiToolStripMenuItem.Text = "Yeni Müşteri Girişi";
+            // 
+            // mevcutOlanMüşterilerToolStripMenuItem
+            // 
+            this.mevcutOlanMüşterilerToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("mevcutOlanMüşterilerToolStripMenuItem.Image")));
+            this.mevcutOlanMüşterilerToolStripMenuItem.Name = "mevcutOlanMüşterilerToolStripMenuItem";
+            this.mevcutOlanMüşterilerToolStripMenuItem.Size = new System.Drawing.Size(202, 38);
+            this.mevcutOlanMüşterilerToolStripMenuItem.Text = "Mevcut Olan Müşteriler";
+            // 
+            // odalarToolStripMenuItem
+            // 
+            this.odalarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.doluOdalarToolStripMenuItem,
+            this.boşOdalarToolStripMenuItem});
+            this.odalarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("odalarToolStripMenuItem.Image")));
+            this.odalarToolStripMenuItem.Name = "odalarToolStripMenuItem";
+            this.odalarToolStripMenuItem.Size = new System.Drawing.Size(104, 36);
+            this.odalarToolStripMenuItem.Text = "Oda İşlemleri";
+            // 
+            // doluOdalarToolStripMenuItem
+            // 
+            this.doluOdalarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.boşalacakOdalarToolStripMenuItem});
+            this.doluOdalarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("doluOdalarToolStripMenuItem.Image")));
+            this.doluOdalarToolStripMenuItem.Name = "doluOdalarToolStripMenuItem";
+            this.doluOdalarToolStripMenuItem.Size = new System.Drawing.Size(152, 38);
+            this.doluOdalarToolStripMenuItem.Text = "Dolu Odalar";
+            // 
+            // boşalacakOdalarToolStripMenuItem
+            // 
+            this.boşalacakOdalarToolStripMenuItem.Name = "boşalacakOdalarToolStripMenuItem";
+            this.boşalacakOdalarToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.boşalacakOdalarToolStripMenuItem.Text = "Boşalacak Odalar";
+            // 
+            // boşOdalarToolStripMenuItem
+            // 
+            this.boşOdalarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kişilikOdalarToolStripMenuItem,
+            this.kişilikOdalarToolStripMenuItem1});
+            this.boşOdalarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("boşOdalarToolStripMenuItem.Image")));
+            this.boşOdalarToolStripMenuItem.Name = "boşOdalarToolStripMenuItem";
+            this.boşOdalarToolStripMenuItem.Size = new System.Drawing.Size(152, 38);
+            this.boşOdalarToolStripMenuItem.Text = "Boş Odalar";
+            // 
+            // kişilikOdalarToolStripMenuItem
+            // 
+            this.kişilikOdalarToolStripMenuItem.Name = "kişilikOdalarToolStripMenuItem";
+            this.kişilikOdalarToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.kişilikOdalarToolStripMenuItem.Text = "2 Kişilik Odalar";
+            // 
+            // kişilikOdalarToolStripMenuItem1
+            // 
+            this.kişilikOdalarToolStripMenuItem1.Name = "kişilikOdalarToolStripMenuItem1";
+            this.kişilikOdalarToolStripMenuItem1.Size = new System.Drawing.Size(154, 22);
+            this.kişilikOdalarToolStripMenuItem1.Text = "3 Kişilik Odalar";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("çıkışToolStripMenuItem.Image")));
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(95, 36);
+            this.çıkışToolStripMenuItem.Text = "Çıkış İşlemi";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // müşteriAramaToolStripMenuItem
+            // 
+            this.müşteriAramaToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("müşteriAramaToolStripMenuItem.Image")));
+            this.müşteriAramaToolStripMenuItem.Name = "müşteriAramaToolStripMenuItem";
+            this.müşteriAramaToolStripMenuItem.Size = new System.Drawing.Size(202, 38);
+            this.müşteriAramaToolStripMenuItem.Text = "Müşteri Arama";
+            // 
+            // anasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1362, 648);
+            this.Controls.Add(this.menuler);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuler;
+            this.Name = "anasayfa";
+            this.Text = "Otel Otomasyon Version1.1";
+            this.menuler.ResumeLayout(false);
+            this.menuler.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuler;
+        private System.Windows.Forms.ToolStripMenuItem müşteriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniMüşteriGirişiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem odalarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem doluOdalarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boşalacakOdalarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boşOdalarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kişilikOdalarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kişilikOdalarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mevcutOlanMüşterilerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem müşteriAramaToolStripMenuItem;
+    }
+}
+
---
+++ otel_otomasyon/anaform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace otel_otomasyon
+{
+    public partial class anasayfa : Form
+    {
+        public anasayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Çıkış Yapacaksınız Eminmisiniz?", "Çıkış İşlemi", MessageBoxButtons.YesNo, MessageBoxIcon.Stop) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+           }
+             
+    }
+}
